0138240ec9acd3045268f6ee50d759886721dea3
fqroldan <fr673@nyu.edu>
2022-10-15 11:04:10 -0400

    improvements to comp_argbond

--- Codes/compstats.jl
+++ Codes/compstats.jl
@@ -1,20 +1,21 @@
 function comp_argbond(dd::DebtMod; show_simul=false)
-
-    mpe_simul!(dd, K=2, simul=show_simul)
+    @assert dd.pars[:α] == 0 && dd.pars[:τ] <= min(dd.gr[:y])
+    
+    mpe_simul!(dd, simul=show_simul)
 
     Ny = length(dd.gr[:y])
     v_noncont = dd.v[:V][1, ceil(Int, Ny/2)]
 
     dd.pars[:α] = 1
+    dd.pars[:min_q] = 0
 
-    mpe_simul!(dd, K=2, initialrep = false, simul=false)
+    mpe_simul!(dd, initialrep = false, simul=false)
 
     v_linear = dd.v[:V][1, ceil(Int, Ny/2)]
     
     dd.pars[:τ] = 1
-    dd.pars[:min_q] = 0
     
-    mpe_simul!(dd, K=2, initialrep = false, simul=false)
+    mpe_simul!(dd, initialrep = false, simul=false)
 
     v_threshold = dd.v[:V][1, ceil(Int, Ny/2)]
 
