9e19f7aa13382eefb31714e6338d67da0ef679d9
fqroldan <fr673@nyu.edu>
2022-10-16 19:36:09 -0400

    fixed mutable comp_argbond

--- Codes/compstats.jl
+++ Codes/compstats.jl
@@ -1,7 +1,6 @@
 function comp_argbond(dd::DebtMod; show_simul=false, DEP=false)
     @assert dd.pars[:α] == 0 && dd.pars[:τ] <= minimum(dd.gr[:y])
 
-    save("dd_comp.jld2", "dd", dd)
     mpe_simul!(dd, simul=show_simul)
 
     Ny = length(dd.gr[:y])
@@ -24,7 +23,6 @@ function comp_argbond(dd::DebtMod; show_simul=false, DEP=false)
         print("DEP with linear: $DEP_linear\n")
     end
 
-
     dd.pars[:τ] = 1
 
     mpe_simul!(dd, maxiter=500, K=8, initialrep=false, simul=false)
@@ -34,8 +32,6 @@ function comp_argbond(dd::DebtMod; show_simul=false, DEP=false)
         _, DEP_threshold = simul_dist(dd, K=1_000, burn_in=1_000, T=240)
         print("DEP with threshold: $DEP_threshold\n")
     end
-
-    dd = load("dd_comp.jld2", "dd")
 
     return v_noncont, v_linear, v_threshold
 end
@@ -47,11 +43,10 @@ function comp_t5(dd::DebtMod)
     save("dd_comp_theta.jld2", "dd", dd)
     rob_n, rob_l, rob_t = comp_argbond(dd, show_simul=true, DEP = true)
 
+    dd = load("dd_comp_theta.jld2", "dd")
     dd.pars[:θ] = 0
     print("Solving same model with rational expectations\n")
     rat_n, rat_l, rat_t = comp_argbond(dd, show_simul=true)
-
-    dd = load("dd_comp_theta.jld2", "dd")
 
     return rob_n, rob_l, rob_t, rat_n, rat_l, rat_t
 end
