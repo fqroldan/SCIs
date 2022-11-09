30c502c265b6aefc19bcd00be78d51db3bec9c95
fqroldan <fr673@nyu.edu>
2022-11-09 14:45:30 -0500

    added spread decomposition

--- Codes/spreads.jl
+++ Codes/spreads.jl
@@ -93,3 +93,79 @@ end
 
 get_spread(q, κ::Number) = κ * (1/q - 1)
 get_spread(q, dd::DebtMod) = get_spread(q, dd.pars[:κ])
+
+
+function spread_decomp(dd::DebtMod)
+    ρ, ℏ, θ, βL = (dd.pars[sym] for sym in (:ρ, :ℏ, :ψ, :r, :θ, :βL))
+
+    # Interpola el precio de la deuda (para mañana)
+    itp_qd = make_itp(dd, dd.qD);
+    itp_q = make_itp(dd, dd.q);
+
+    q_RE = similar(dd.q)
+    qθ_def = similar(dd.q)
+    qθ_cont = similar(dd.q)
+
+    for (jbp, bpv) in enumerate(dd.gr[:b]), (jy, yv) in enumerate(dd.gr[:y])
+        Eq_RE = 0.0
+        E_P     = 0.0
+        E_PD    = 0.0
+        E_M     = 0.0
+        E_d     = 0.0
+        E_Md    = 0.0
+        E_MP    = 0.0
+        E_MPD   = 0.0
+        
+        sum_sdf = 0.0
+        
+        for (jyp, ypv) in enumerate(dd.gr[:y])
+            prob = dd.P[:y][jy, jyp]
+
+            prob_def = dd.v[:prob][jbp, jyp]
+        
+            if θ > 1e-3
+                sdf_R = exp(-θ * dd.vL[jbp, jyp, 1])
+                sdf_D = exp(-θ * dd.vL[jbp, jyp, 2])
+            else
+                sdf_R = 1.
+                sdf_D = 1.
+            end
+        
+            coupon = coupon_rate(ypv, dd)
+        
+            # Si el país tiene acceso a mercados, emite y puede hacer default mañana
+            bpp = dd.gb[jbp, jyp]
+
+            P  = coupon + (1-ρ) * itp_q(bpp, jyp)
+            PD = (1-ℏ) * itp_qd((1-ℏ) * bpv, jyp)
+
+            Eq_RE += prob * ((1 - prob_def) * P + (prob_def) * PD)
+
+            E_P   += prob * P
+
+            E_PD  += prob * PD
+
+            sum_sdf += prob * (prob_def * sdf_D + (1-prob_def) * sdf_R)
+            E_M   += prob * βL * (prob_def * sdf_D + (1-prob_def) * sdf_R)
+
+            E_d   += prob * prob_def * 1
+
+            E_Md  += prob * βL * (prob_def * sdf_D + (1-prob_def) * 0)
+
+            E_MP  += prob * P * βL * (prob_def * sdf_D + (1-prob_def) * sdf_R)
+            E_MPD += prob * PD* βL * (prob_def * sdf_D + (1-prob_def) * sdf_R)
+        end
+
+        cov_M_d  = (E_Md - E_M * E_d) / sum_sdf
+        cov_M_P  = (E_MP - E_M * E_P) / sum_sdf
+        cov_M_PD = (E_MPD- E_M * E_PD)/ sum_sdf
+        
+        q_RE[jbp, jy] = E_M / sum_sdf * Eq_RE
+
+        qθ_def[jbp, jy] = - (E_P - E_PD) * cov_M_d
+
+        qθ_cont[jbp, jy] = (1-E_d) * cov_M_P + E_d * cov_M_PD
+    end
+
+    return q_RE, qθ_def, qθ_cont
+end
