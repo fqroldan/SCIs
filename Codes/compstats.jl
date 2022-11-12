ceef6b3938125bc623a93caeecc4a9c45f71af17
fqroldan <fr673@nyu.edu>
2022-11-11 19:43:09 -0500

    same T and K from other simul

--- Codes/compstats.jl
+++ Codes/compstats.jl
@@ -4,9 +4,9 @@ function comp_argbond(dd::DebtMod; showtable=false, smalltable=!showtable, DEP=f
     mpe_simul!(dd, maxiter=500, K=0, simul=false)
 
     Random.seed!(25)
-    w, t, m, ϵvv, ξvv = calib_targets(dd, cond_K=7_500, uncond_K=10_000, smalltable=smalltable, showtable=showtable)
+    w, t, m, ϵvv, ξvv = calib_targets(dd, smalltable=smalltable, showtable=showtable)
 
-
+    
     Ny = length(dd.gr[:y])
     v_noncont = welfare(dd)
     c_noncont = cons_equiv(v_noncont, dd)
