424529e6514ccd5811e9ead3a1898df25974da13
fqroldan <fr673@nyu.edu>
2022-12-03 10:35:56 -0500

    added replication for 2-period model

---
+++ Codes/replication_2p.jl
@@ -0,0 +1,30 @@
+include("sci_2p_v17.jl")
+
+# Enter folder to save plots
+folder = "Codes/temp"
+
+Nθ = 9
+Nz = 5001
+
+rb, rf, γ, Δ, g = default_params(2)
+
+# Figure 2: distorted probabilities with noncontingent
+fig2 = plot_probs(rb=rb, rf = rf, γ = γ, Δ=Δ, g=g, bench="noncontingent", Nθ = Nθ, Nz = Nz, reopt=false, slides=false, dark=false, lw=3)
+
+savefig(fig2, folder * "/fig2.pdf", width = 864, height = 378)
+
+# Figure 3: distorted probabilities with threshold
+fig3 = plot_probs(rb=rb, rf = rf, γ = γ, Δ=Δ, g=g, bench="threshold", Nθ = Nθ, Nz = Nz, reopt=false, slides=false, dark=false, lw=3)
+
+savefig(fig3, folder * "/fig3.pdf", width = 864, height = 378)
+
+# Figure 4: Optimal design for each lender
+fig4 = plots_arrow(Nz = 101, Nθ = 5, slides=false, dark=false, lw=3.5)
+
+savefig(fig4, folder * "/fig4.pdf", width = 864, height = 378)
+
+
+# Figure 5: Spreads and welfare
+fig5 = plot_parametric(Nz = Nz, slides=false, dark=false)
+
+savefig(fig5, folder * "/fig5.pdf", width = 1080, height = 621)
